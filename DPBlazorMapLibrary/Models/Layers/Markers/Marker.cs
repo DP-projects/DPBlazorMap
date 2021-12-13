3cd1d26576c4da06d2ce1c4a38e61c844c9226cd
Denis Polagaev <polagaev.denis@inbox.ru>
2021-12-13 03:16:27 +0300

    add layer group and future group

--- DPBlazorMapLibrary/Models/Layers/Markers/Marker.cs
+++ DPBlazorMapLibrary/Models/Layers/Markers/Marker.cs
@@ -5,12 +5,12 @@ namespace DPBlazorMapLibrary
 {
     public class Marker : InteractiveLayer
     {
-        private const string GetLatLngJsFunction = "getLatLng";
-        private const string SetLatLngJsFunction = "setLatLng";
-        private const string SetZIndexOffsetJsFunction = "setZIndexOffset";
-        private const string GetIconJsFunction = "getIcon";
-        private const string SetIconJsFunction = "setIcon";
-        private const string SetOpacityJsFunction = "setOpacity";
+        private const string _getLatLngJsFunction = "getLatLng";
+        private const string _setLatLngJsFunction = "setLatLng";
+        private const string _setZIndexOffsetJsFunction = "setZIndexOffset";
+        private const string _getIconJsFunction = "getIcon";
+        private const string _setIconJsFunction = "setIcon";
+        private const string _setOpacityJsFunction = "setOpacity";
 
         public Marker(IJSObjectReference jsReference, IEventedJsInterop eventedJsInterop)
         {
@@ -20,32 +20,32 @@ namespace DPBlazorMapLibrary
 
         public async Task<LatLng> GetLatLng()
         {
-            return await JsReference.InvokeAsync<LatLng>(GetLatLngJsFunction);
+            return await JsReference.InvokeAsync<LatLng>(_getLatLngJsFunction);
         }
 
         public async Task<IJSObjectReference> SetLatLng(LatLng latLng)
         {
-            return await JsReference.InvokeAsync<IJSObjectReference>(SetLatLngJsFunction, latLng);
+            return await JsReference.InvokeAsync<IJSObjectReference>(_setLatLngJsFunction, latLng);
         }
 
         public async Task<IJSObjectReference> SetZIndexOffset(int number)
         {
-            return await JsReference.InvokeAsync<IJSObjectReference>(SetZIndexOffsetJsFunction, number);
+            return await JsReference.InvokeAsync<IJSObjectReference>(_setZIndexOffsetJsFunction, number);
         }
 
         public async Task<Icon> GetIcon()
         {
-            return await JsReference.InvokeAsync<Icon>(GetIconJsFunction);
+            return await JsReference.InvokeAsync<Icon>(_getIconJsFunction);
         }
 
         public async Task<IJSObjectReference> SetIcon(Icon icon)
         {
-            return await JsReference.InvokeAsync<IJSObjectReference>(SetIconJsFunction, icon);
+            return await JsReference.InvokeAsync<IJSObjectReference>(_setIconJsFunction, icon);
         }
 
         public async Task<IJSObjectReference> SetOpacity(double number)
         {
-            return await JsReference.InvokeAsync<IJSObjectReference>(SetOpacityJsFunction, number);
+            return await JsReference.InvokeAsync<IJSObjectReference>(_setOpacityJsFunction, number);
         }
     }
 }
