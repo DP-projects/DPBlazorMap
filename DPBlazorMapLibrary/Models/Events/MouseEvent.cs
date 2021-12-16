0f95e2fbffeacd3abbd658a19374936d17aba30e
Denis Polagaev <polagaev.denis@inbox.ru>
2021-12-16 14:59:49 +0700

    Update MouseEvent.cs

--- DPBlazorMapLibrary/Models/Events/MouseEvent.cs
+++ DPBlazorMapLibrary/Models/Events/MouseEvent.cs
@@ -2,6 +2,19 @@
 {
     public class MouseEvent : Event
     {
+        /// <summary>
+        /// The geographical point where the mouse event occurred.
+        /// </summary>
         public LatLng LatLng { get; set; }
+
+        /// <summary>
+        /// Pixel coordinates of the point where the mouse event occurred relative to the map layer.
+        /// </summary>
+        public Point LayerPoint { get; set; }
+
+        /// <summary>
+        /// Pixel coordinates of the point where the mouse event occurred relative to the map сontainer.
+        /// </summary>
+        public Point ContainerPoint { get; set; }
     }
 }
